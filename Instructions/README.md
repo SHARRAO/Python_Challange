e02019138c37862e1bfc34f8b36f10b707477986
sharmila <sharrao08@gmail.com>
2020-08-22 20:10:14 -0400

    PyBank_done

--- Instructions/README.md
+++ Instructions/README.md
@@ -1,3 +1,4 @@
+
 # Python Homework - Py Me Up, Charlie
 
 ## Background
--- PyBank/Main.py
+++ PyBank/Main.py
@@ -11,6 +11,10 @@ current_month_p_l=0
 previous_month_p_l=0
 change_in_twomonths=0
 sumchange=0
+gratest_increase=0
+greatest_dec=0
+date_inc=""
+date_dec=""
 #open the csv
 
 with open(budget_data_path) as csvfile:
@@ -20,17 +24,30 @@ with open(budget_data_path) as csvfile:
        # print(r[1])
         
         if r[0] != "Date":
+            Tot_mon=Tot_mon+1 #---(code  for total number of months ) 
+            Tot_Amount =Tot_Amount + float(r[1]) #---(Code for total amount )
+            
             current_month_p_l= float(r[1])
             if Tot_mon > 1:
-                change_in_twomonths = (previous_month_p_l -current_month_p_l)
-                sumchange =+ change_in_twomonths
+                change_in_twomonths = (current_month_p_l - previous_month_p_l)
+                sumchange = sumchange + change_in_twomonths
+                if change_in_twomonths>0:
+                    if change_in_twomonths > gratest_increase: #---(greatest increase in profits)
+                        gratest_increase= change_in_twomonths
+                        date_inc= r[0]
+                else: 
+                    if change_in_twomonths < greatest_dec: #--(The greatest decrease in losses) 
+                        greatest_dec = change_in_twomonths
+                        date_dec= r[0]
+                
             previous_month_p_l= current_month_p_l
 
-            Tot_mon=Tot_mon+1 #(code  for total number of months ) 
-            Tot_Amount =Tot_Amount + float(r[1]) #(Code for total amount )
     print("Total number of months is: " + str(Tot_mon))
     print("Total: $  "+ str(Tot_Amount)) 
-    print("Average Change: " + str((sumchange-867884)/(Tot_mon-1)))   
+    print("Average Change: " + str((sumchange)/(Tot_mon-1)))#---(caliculate the average)
+    print("Greatest  Increase in profits:  "+ date_inc +"  ($"+ str(gratest_increase)+")") 
+    print("Greatest Decrease in losses:  " + date_dec + "  ($"+ str(greatest_dec)+")") 
+
     
  
-#--------3------ caliculate the average...
+
